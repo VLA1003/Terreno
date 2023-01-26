903d3b0b5dda48de45fde3660b9478d5e6187425
paulamgonzalez <11alvaro11alonso@gmail.com>
2023-01-26 11:53:42 +0000

    Quinto commit

--- Assets/Scripts/MovimientoNave.cs
+++ Assets/Scripts/MovimientoNave.cs
@@ -7,6 +7,7 @@ public class MovimientoNave : MonoBehaviour
     [SerializeField]
     public float velocidadNave = 1f;
     public float velocidadTurbo = 10f;
+    public float velocidadFreno = 5f;
     public float velocidadRotacionVerticalNave = 50f;
     public float velocidadRotacionHorizontalNave = 50f;
     public float velocidadRotacionDiagonalNave = 10f;
@@ -14,7 +15,7 @@ public class MovimientoNave : MonoBehaviour
     void Update()
     {
         // Movimiento y rotación de la nave.
-        float rotacionX = Input.GetAxis("Vertical") * Time.deltaTime * velocidadRotacionVerticalNave;
+        float rotacionX = -Input.GetAxis("Vertical") * Time.deltaTime * velocidadRotacionVerticalNave;
         float rotacionY = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadRotacionHorizontalNave;
         float rotacionZ = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadRotacionDiagonalNave;
         transform.position += -transform.forward * Time.deltaTime * velocidadNave;
@@ -25,21 +26,26 @@ public class MovimientoNave : MonoBehaviour
         // Sistema de turbo de la nave.
         if (Input.GetButtonDown("Jump"))
         {
-            velocidadNave = velocidadNave + 10;
+            velocidadNave = velocidadNave + velocidadTurbo;
         }
         else if (Input.GetButtonUp("Jump"))
         {
-            velocidadNave = velocidadNave - 10;
+            velocidadNave = velocidadNave - velocidadTurbo;
         }
 
         //Sistema de freno de la nave.
         if (Input.GetButtonDown("Enable Debug Button 1"))
         {
-            velocidadNave = velocidadNave - 5;
+            velocidadNave = velocidadNave - velocidadFreno;
         }
         else if (Input.GetButtonUp("Enable Debug Button 1"))
         {
-            velocidadNave = velocidadNave + 5;
+            velocidadNave = velocidadNave + velocidadFreno;
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            transform.Rotate(0, 0, 0, Space.Self);
         }
 
     }
