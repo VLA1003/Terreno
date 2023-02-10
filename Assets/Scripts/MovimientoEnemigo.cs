82e6f47e49f0f443860798c148cf55f686080579
paulamgonzalez <11alvaro11alonso@gmail.com>
2023-02-10 09:11:39 +0000

    Séptimo commit
    //Enemigos, sistema de movimiento (con puntos de ruta) y colisión con proyectiles del jugador (para que el enemigo muera al dispararle).

---
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    [SerializeField]
+    public Transform[] puntosRuta;
+    public Transform puntoRutaActual;
+    public float velocidadEnemigo = 2;
+    public int numeroPuntoRuta = 0;
+
+    private void Start()
+    {
+        gameObject.transform.position = puntosRuta[numeroPuntoRuta].transform.position;
+    }
+
+    //Movimiento constante de la nave enemiga.
+    private void Update()
+    {
+        MovimientoNaveEnemiga();
+    }
+
+    //Movimiento de la nave enemiga.
+    public void MovimientoNaveEnemiga()
+    {
+        puntoRutaActual = puntosRuta[numeroPuntoRuta];
+        if (numeroPuntoRuta <= puntosRuta.Length - 1)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, puntosRuta[numeroPuntoRuta].transform.position, velocidadEnemigo * Time.deltaTime);
+            if (transform.position == puntosRuta[numeroPuntoRuta].transform.position)
+            {
+                numeroPuntoRuta += 1;
+            }
+
+        if (numeroPuntoRuta == puntosRuta.Length)
+            {
+                numeroPuntoRuta = 0;
+                gameObject.transform.position = puntosRuta[numeroPuntoRuta].transform.position;
+            }
+            Vector3 posicionPuntoRuta = puntoRutaActual.position - transform.position;
+            gameObject.transform.rotation = Quaternion.LookRotation(-posicionPuntoRuta);
+
+        }
+    }
+
+    //Detección del proyectil para destrucción del enemigo.
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "projectile")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
